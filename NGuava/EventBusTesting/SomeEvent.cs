d94ad4727e45f0a6bb29319dabacdeb024c57f02
Phil Scrace <phil.scrace@aveva.com>
2024-07-29 15:41:19 +0100

    .NET 8.0 whole solution upgrade

---
+++ NGuava/EventBusTesting/SomeEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventBusTests
+{
+    public class SomeEvent
+    {
+        public SomeEvent() { }
+        public void UpdateSomeEventValue() {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NGuava/EventBusTests/Controller.cs
@@ -0,0 +1,23 @@
+﻿using NGuava;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBusTests
+{
+    public  class Controller
+    {
+        public Controller(IEventBus eventBus)
+        {
+            eventBus.Register(this);
+        }
+
+        [Subscribe]
+        public void OnDoSomeThing(SomeEvent evnt)
+        {
+            evnt.UpdateSomeEventValue();
+        }
+    }
+}
